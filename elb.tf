6099ec5788d5fc4a86e2ea525d36f7256c6682a0
Mohit Mishra <99587336+mmohit1271@users.noreply.github.com>
2025-03-18 22:36:47 +0530

    Update elb.tf

--- elb.tf
+++ elb.tf
@@ -1,6 +1,6 @@
 resource "aws_elb" "bar" {
   name               = "mmishra-terraform-elb"
-  availability_zones = ["eu-west-1a", "eu-west-1b"]
+  availability_zones = ["ap-south-1a", "ap-south-1b"]
 
   listener {
     instance_port     = 80
